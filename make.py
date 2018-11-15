6c67adb54e6d4a367f09b436f4bf39d3dcbf7a84
gabrieldemarmiesse <gabreil.de-marmiesse@valeo.com>
2018-11-15 12:08:11 +0100

    The image works well.

--- make.py
+++ make.py
@@ -5,6 +5,14 @@ def build(version_name):
     cmd = f'sudo docker build -t 192.168.50.123:5000/gpustat:{version_name} .'
     cmd += f' && sudo docker push 192.168.50.123:5000/gpustat:{version_name}'
     os.system(cmd)
+    os.system('docker run --rm --runtime=nvidia -t --pid=host -v /etc/passwd:/etc/passwd -v '
+              '/var/run/docker.sock:/var/run/docker.sock 192.168.50.123:5000/gpustat:stable '
+              'gpustat')
+
+# execute with
+# docker run --rm -t --runtime=nvidia -it -d --name=mygpustat --pid=host -v /etc/passwd:/etc/passwd -v /var/run/docker.sock:/var/run/docker.sock 192.168.50.123:5000/gpustat:stable
+# connect with
+# docker exec -it mygpustat gpustat -P
 
 
 if __name__ == '__main__':
