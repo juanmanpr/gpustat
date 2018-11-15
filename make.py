ccd05acbdf65db9f6e75eb3e779593e28b76f31d
gabrieldemarmiesse <gabreil.de-marmiesse@valeo.com>
2018-11-15 11:03:47 +0100

    Renamed again.

--- make.py
+++ make.py
@@ -1,15 +1,7 @@
 import os
-import shutil
 
 
 def build(version_name):
-    docker_dir = os.path.dirname(os.path.realpath(__file__))
-    try:
-        shutil.rmtree(docker_dir + 'gpustat')
-    except:
-        pass
-    shutil.copytree(docker_dir + '/../', docker_dir + 'gpustat')
-
     cmd = f'sudo docker build -t 192.168.50.123:5000/gpustat:{version_name} .'
     cmd += f' && sudo docker push 192.168.50.123:5000/gpustat:{version_name}'
     os.system(cmd)
