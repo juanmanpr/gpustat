54c562abeef5c614b9abfbda8bd4ff0356a9b550
gabrieldemarmiesse <gabreil.de-marmiesse@valeo.com>
2018-11-30 14:23:31 +0100

    Changed the ip and removed the sudo.

--- make.py
+++ make.py
@@ -2,15 +2,15 @@ import os
 
 
 def build(version_name):
-    cmd = f'sudo docker build -t 192.168.50.123:5000/gpustat:{version_name} .'
-    cmd += f' && sudo docker push 192.168.50.123:5000/gpustat:{version_name}'
+    cmd = f'docker build -t 192.168.50.175:5000/gpustat:{version_name} .'
+    cmd += f' && docker push 192.168.50.175:5000/gpustat:{version_name}'
     os.system(cmd)
-    os.system('docker run --rm --runtime=nvidia -t --pid=host -v /etc/passwd:/etc/passwd -v '
-              '/var/run/docker.sock:/var/run/docker.sock 192.168.50.123:5000/gpustat:stable '
-              'gpustat')
+    #os.system('docker run --rm --runtime=nvidia -t --pid=host -v /etc/passwd:/etc/passwd -v '
+    #          '/var/run/docker.sock:/var/run/docker.sock 192.168.50.175:5000/gpustat:stable '
+    #          'gpustat')
 
 # execute with
-# docker run --rm -t --runtime=nvidia -it -d --name=mygpustat --pid=host -v /etc/passwd:/etc/passwd -v /var/run/docker.sock:/var/run/docker.sock 192.168.50.123:5000/gpustat:stable
+# docker run --rm -t --runtime=nvidia -it -d --name=mygpustat --pid=host -v /etc/passwd:/etc/passwd -v /var/run/docker.sock:/var/run/docker.sock 192.168.50.175:5000/gpustat:stable
 # connect with
 # docker exec -it mygpustat gpustat -P
 
